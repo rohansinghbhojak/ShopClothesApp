b7bc7d00946a8633684d74ebfb3f3c481a357268
ROHAN SINGH BHOJAK <bhojakrohansingh@gmail.com>
2025-11-23 00:48:29 +0530

    Initial Commit

---
+++ ShopClothesApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShopClothesApp
+//
+//  Created by Rohan Bhojak  on 23/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShopClothesApp/ShopClothesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopClothesAppApp.swift
+//  ShopClothesApp
+//
+//  Created by Rohan Bhojak  on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopClothesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
